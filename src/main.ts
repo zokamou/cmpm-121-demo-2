bba4ac0cfd8b545e813993a7539d18c053c27a85
zfeller <zfeller@ucsc.edu>
2024-10-16 10:19:49 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
-const APP_NAME = "New Game";
+const APP_NAME = "";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+
+let gameName = document.createElement('h1');
+gameName.innerText = "Sketchpad";
+gameName.className = 'game-name';
+app.appendChild(gameName);
+
+let canvas = document.createElement('canvas');
+canvas.className = 'canvas';
+app.appendChild(canvas);
+
